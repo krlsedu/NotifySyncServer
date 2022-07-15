a77f4b63e8f9b058d1fa9389e9147ed7579522f5
Carlos Eduardo Duarte Schwalm <krlsedu@gmail.com>
2022-07-15 17:25:01 -0300

    Bugfix

--- src/main/java/com/csctracker/notifysyncserver/controller/ConfigsController.java
+++ src/main/java/com/csctracker/notifysyncserver/controller/ConfigsController.java
@@ -3,6 +3,7 @@ package com.csctracker.notifysyncserver.controller;
 import com.csctracker.dto.Conversor;
 import com.csctracker.model.Configs;
 import com.csctracker.notifysyncserver.dto.ConfigsDTO;
+import com.csctracker.securitycore.service.UserInfoService;
 import com.csctracker.service.ConfigsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -16,20 +17,23 @@ public class ConfigsController {
 
     private final Conversor<Configs, ConfigsDTO> conversor;
 
+    private final UserInfoService userInfoService;
+
     @Autowired
-    public ConfigsController(ConfigsService configsService) {
+    public ConfigsController(ConfigsService configsService, UserInfoService userInfoService) {
         this.configsService = configsService;
+        this.userInfoService = userInfoService;
         this.conversor = new Conversor<>(Configs.class, ConfigsDTO.class);
     }
 
     @GetMapping("/configs")
     private ResponseEntity<ConfigsDTO> getConfigs() {
-        return new ResponseEntity<>(conversor.toD(configsService.getConfigByUser()), HttpStatus.OK);
+        return new ResponseEntity<>(conversor.toD(configsService.getConfigByUser(userInfoService.getUser())), HttpStatus.OK);
     }
 
     @PostMapping(value = "/configs", consumes = "application/json")
     @ResponseStatus(HttpStatus.ACCEPTED)
     private void setConfigs(@RequestBody ConfigsDTO configsDTO) {
-        configsService.save(conversor.toE(configsDTO));
+        configsService.save(conversor.toE(configsDTO), userInfoService.getUser());
     }
 }
